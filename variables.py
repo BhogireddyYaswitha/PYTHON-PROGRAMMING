0ded0d5dcf926a308912b0880ceee3afb0531c5b
YASWITHA BHOGIREDDY <yashubhogireddy@gmail.com>
2025-07-14 21:36:29 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,65 @@
+'''yaswitha=20
+print(yaswitha)
+
+a=b=c=100
+print(a,b,c)
+
+a=1,2,3
+print(a)
+
+#we can use
+yaswitha=10000
+_yaswitha=10
+yashu1234=197
+yaswitha="yaswitha"
+
+#we cannot use
+12yashu=159
+@yashu=87
+
+yaswitha!=56
+print(yaswitha!)
+
+name=12
+Name=1
+NAME=2
+print(name)
+
+a=10
+print(id(a))'''
+
+#int() float() bool()
+
+'''a=12
+b=2.3
+c=True
+d=False
+e=2+3j
+print(type(a),type(b),type(c),type(d),type(e))'''
+
+#type conversions
+
+'''a=12
+print(float(a))
+
+b=12.56
+print(int(b))'''
+
+#input fun
+'''a=int(input("enter someting"))
+print(a)'''
+
+#compond interest and simple interest
+
+'''p=10000
+t=2
+r=5
+si=(p*t*r)/100
+print(si)'''
+
+# program on (a+b)**2
+
+a=int(input("enter a number"))
+b=int(input("enter a number"))
+result=(a+b)**2
+print(result)
