8763f6f745f3000e59288010ed8c5a2624326940
YASWITHA BHOGIREDDY <yashubhogireddy@gmail.com>
2025-07-15 21:25:53 +0530

    Add files via upload

---
+++ conditional_statement.py
@@ -0,0 +1,58 @@
+'''if 2<1: 
+    print("This is if")
+elif 2<1:
+    print("This is elif")
+else:
+    print("This is else")'''
+    
+###short hand if
+#if 2>1:print("This is if")    
+
+'''a=1
+b=2
+print("This is if") if b>a else print("This is else")'''
+
+##while conditional statement
+'''while False:
+    print("this is loop")
+else:
+    print("else")'''  
+    
+'''t=20
+while t<40:
+    print("this is while",t) 
+    t+=1'''
+    
+#for i in iterators:
+#    statement
+
+'''for i in "kiran":
+    print(i)'''
+
+'''for a in range(1,10,3):
+    print(a)'''
+    
+'''for j in "python":
+    if j=="t":
+        continue
+    print(j)'''
+
+'''if True:
+    pass'''
+    
+'''for i in range(1,1001):
+    for j in range(1,11):
+        print(i*j,end=" ")
+    print()'''
+    
+user="chinnu"
+password="chinnu341"
+user_name=input("enter a user")
+password_name=input("enter a password")
+if user==user_name and password==password_name:
+    print("success")
+else:
+    print("enter wrong")                            
+     
+              
+        
